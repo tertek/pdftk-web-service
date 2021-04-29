52068e6fb9e9899946f2e4f58e96aa978b689bfb
tertek <ekin.tertemiz@swisstph.ch>
2021-04-29 16:35:27 +0200

    Cleanip

--- index.php
+++ index.php
@@ -1,41 +1,9 @@
 <?php
 header("Content-Type: text/plain");
 
-echo "\nTask 1: whoami\n";
-$output=null;
-$retval=null;
-exec('whoami', $output, $retval);
-echo "Returned with status $retval and output:\n";
-print_r($output);
-
-
-echo "\nTask 2: pdftk\n";
+echo "pdftk\n";
 $output=null;
 $retval=null;
 exec('pdftk', $output, $retval);
-echo "Returned with status $retval and output:\n";
-print_r($output);
-
-
-echo "\nTask 3: /bin/bash pdftk\n";
-$output=null;
-$retval=null;
-exec('/bin/bash pdftk', $output, $retval);
-echo "Returned with status $retval and output:\n";
-print_r($output);
-
-
-echo "\nTask 4: ls -la\n";
-$output=null;
-$retval=null;
-exec('ls -la', $output, $retval);
-echo "Returned with status $retval and output:\n";
-print_r($output);
-
-
-echo "\nTask 5: vendor/pdftk/bin/pdftk \n";
-$output=null;
-$retval=null;
-exec('vendor/pdftk/bin/pdftk', $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
