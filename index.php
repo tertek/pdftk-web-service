f9916b4b0025743f6805d03f2cf73f17c101f7f2
tertek <ekin.tertemiz@swisstph.ch>
2021-04-29 16:17:19 +0200

    Add test 3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: text/plain");
 
-echo "\nTask 1";
+echo "\nTask 1: whoami\n";
 $output=null;
 $retval=null;
 exec('whoami', $output, $retval);
@@ -9,9 +9,17 @@ echo "Returned with status $retval and output:\n";
 print_r($output);
 
 
-echo "\nTask 2";
+echo "\nTask 2: pdftk\n";
 $output=null;
 $retval=null;
 exec('pdftk', $output, $retval);
+echo "Returned with status $retval and output:\n";
+print_r($output);
+
+
+echo "\nTask 3: /bin/bash pdftk\n";
+$output=null;
+$retval=null;
+exec('/bin/bash pdftk', $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
