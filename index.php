9ddfd2765156e9add58a2851f3db277ed15b13e8
tertek <ekin.tertemiz@swisstph.ch>
2021-04-29 16:50:26 +0200

    Add test upload form

--- index.php
+++ index.php
@@ -7,3 +7,21 @@ $retval=null;
 exec('pdftk', $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
+
+header("Content-Type: text/html");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Convert PDF</h1>
+<p>Make PDF readable through FPDM</p>
+<form action="convert.php">
+<input type="file" name="fileToUpload" id="fileToUpload">
+<input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html> 
