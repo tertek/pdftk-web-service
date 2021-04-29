9a0873727ebc9d03780a7619727797defd526440
tertek <ekin.tertemiz@swisstph.ch>
2021-04-29 16:12:51 +0200

    add headers

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
+header("Content-Type: text/plain");
 
+echo "\nTask 1";
 $output=null;
 $retval=null;
 exec('whoami', $output, $retval);
+echo "Returned with status $retval and output:\n";
+print_r($output);
+
+
+echo "\nTask 2";
+$output=null;
+$retval=null;
+exec('pdftk', $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
