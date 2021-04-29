6a34f69610c2f53010911ca56208cbb64cf7e874
tertek <ekin.tertemiz@swisstph.ch>
2021-04-29 16:22:46 +0200

    Add chmod

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 header("Content-Type: text/plain");
 
+chmod("/app/vendor/pdftk/bin/pdftk", "u+x");
+
 echo "\nTask 1: whoami\n";
 $output=null;
 $retval=null;
@@ -29,5 +31,13 @@ echo "\nTask 4: ls -la\n";
 $output=null;
 $retval=null;
 exec('ls -la', $output, $retval);
+echo "Returned with status $retval and output:\n";
+print_r($output);
+
+
+echo "\nTask 5: vendor/pdftk/bin/pdftk \n";
+$output=null;
+$retval=null;
+exec('vendor/pdftk/bin/pdftk', $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
